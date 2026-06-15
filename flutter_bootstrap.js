9fd9f213960614ee8f6c11f1dacff5b963e03d31
nkuchenov-hash <nkuchenov@gmail.com>
2026-06-15 13:55:19 +0000

    Deploying to gh-pages from @ nkuchenov-hash/Counter@d93abc8152fe32b1bcffc6e3d72525af2f064484 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "209936208" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2906241951" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
