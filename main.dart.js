97109f241d64857cf3d1b68cc60221bcbc96d727
nkuchenov-hash <nkuchenov@gmail.com>
2026-07-30 09:42:02 +0000

    Deploying to gh-pages from @ nkuchenov-hash/Counter@2ed37aaf5386ce4f1323e0274c0c0ad952a7e07f 🚀

--- main.dart.js
+++ main.dart.js
@@ -60650,7 +60650,7 @@ s=a.b
 r=s==null?null:B.b.n(s)
 return(r==null?"":r).length===0},
 bYK(a){var s=B.b.n(a)
-return"APP_BUILD commit=29596f0 builtAt=2026-07-29T22:17:39Z"+(s.length!==0?" route="+s:"")},
+return"APP_BUILD commit=2ed37aa builtAt=2026-07-30T09:41:22Z"+(s.length!==0?" route="+s:"")},
 bJ(a,b){var s,r=null,q=$.aEd().ga9()
 if(q==null)return
 q.rP()
@@ -86410,7 +86410,7 @@ m=A.bb(o.h(p,"health_sleep_last_sync_utc_v1"))
 if(m==null)l=null
 else{k=A.m4(m)
 l=k==null?null:k.au()}j=A.fB(o.h(p,"health_sleep_daily_sync_minutes_v1"))
-i=B.d.dA(B.d.aG(j==null?600:j,0,1439))
+i=B.d.dA(B.d.aG(j==null?1260:j,0,1439))
 $.a4A()
 q.a.st(0,new A.ua(n===!0,B.amF,i,!1,!1,l,null,null,null,null,null,null))
 case 3:if(b)$.a4A()
@@ -93981,7 +93981,7 @@ e=A.t(a2).ok.z
 if(e==null)e=a
 else{d=A.t(a2).ax
 c=d.rx
-e=e.b5(c==null?d.k3:c)}return A.k4(a0,a,new A.NR(A.c([new A.mK(q,a,o,a),new A.mK(n,a,new A.Iv(!0,a),a),new A.PL(l,m,a),new A.mK(k,a,j,a),new A.mK(i,a,h,a),new A.mK(g,a,A.z(f+" 29596f0 \xb7 2026-07-29T22:17:39Z",a,a,a,a,a,e,a,a,a,a),a)],p),a),a,a,a,!0)},
+e=e.b5(c==null?d.k3:c)}return A.k4(a0,a,new A.NR(A.c([new A.mK(q,a,o,a),new A.mK(n,a,new A.Iv(!0,a),a),new A.PL(l,m,a),new A.mK(k,a,j,a),new A.mK(i,a,h,a),new A.mK(g,a,A.z(f+" 2ed37aa \xb7 2026-07-30T09:41:22Z",a,a,a,a,a,e,a,a,a,a),a)],p),a),a,a,a,!0)},
 aFq(a,b,c){var s,r,q,p,o,n,m=this,l=null,k=A.L([B.G3,A.k(b,"settings_tab_account"),B.G4,A.k(b,"settings_tab_preferences"),B.yE,A.k(b,"settings_tab_desktop_voice"),B.G5,A.k(b,"settings_tab_notifications"),B.G6,A.k(b,"settings_tab_appearance"),B.G7,A.k(b,"settings_tab_about")],t.OS,t.N),j=A.k(b,"profile_page_title"),i=A.t(a).ok.f
 j=A.z(j,l,l,l,l,l,i==null?l:i.Q7(32,B.ar,1.15),l,l,l,l)
 i=m.ax
@@ -94004,7 +94004,7 @@ p=A.t(a).ok.z
 if(p==null)p=l
 else{o=A.t(a).ax
 n=o.rx
-p=p.b5(n==null?o.k3:n)}p=new A.mK(r,l,A.z(q+" 29596f0 \xb7 2026-07-29T22:17:39Z",l,l,l,l,l,p,l,l,l,l),l)
+p=p.b5(n==null?o.k3:n)}p=new A.mK(r,l,A.z(q+" 2ed37aa \xb7 2026-07-30T09:41:22Z",l,l,l,l,l,p,l,l,l,l),l)
 r=p
 break
 default:r=l}return new A.NR(A.c([j,B.ca,new A.a5m(i,new A.bCk(m),k,l),s,B.eg,r],t.p),l)},
@@ -197317,7 +197317,7 @@ B.amG=new A.Bd(4,"syncing")
 B.amH=new A.Bd(5,"synced")
 B.amI=new A.Bd(6,"noData")
 B.amE=new A.Bd(0,"disabled")
-B.amJ=new A.ua(!1,B.amE,600,!1,!1,null,null,null,null,null,null,null)
+B.amJ=new A.ua(!1,B.amE,1260,!1,!1,null,null,null,null,null,null,null)
 B.amK=new A.acq("height",B.kg,null)
 B.kQ=new A.GZ(0,"push")
 B.kR=new A.GZ(1,"pop")
