9a5ca1c02c5d4ea8ccb2e866834e4638b7000e4b
nkuchenov-hash <nkuchenov@gmail.com>
2026-08-14 01:09:57 +0300

    Switch sleep routes to Google Fit REST runtime

--- pb_hooks/sleep_sync.pb.js
+++ pb_hooks/sleep_sync.pb.js
@@ -1,31 +1,31 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 routerAdd("GET", "/api/sleep-sync/status", function(e) {
-    var sync = require(__hooks + "/sleep_sync_runtime.js");
+    var sync = require(__hooks + "/google_fit_sleep_runtime.js");
     return sync.status(e);
 }, $apis.requireAuth("profiles"));
 
 routerAdd("POST", "/api/sleep-sync/google-fit/connect", function(e) {
-    var sync = require(__hooks + "/sleep_sync_runtime.js");
+    var sync = require(__hooks + "/google_fit_sleep_runtime.js");
     return sync.connect(e);
 }, $apis.requireAuth("profiles"));
 
 routerAdd("GET", "/api/sleep-sync/google-fit/callback", function(e) {
-    var sync = require(__hooks + "/sleep_sync_runtime.js");
+    var sync = require(__hooks + "/google_fit_sleep_runtime.js");
     return sync.callback(e);
 });
 
 routerAdd("POST", "/api/sleep-sync/settings", function(e) {
-    var sync = require(__hooks + "/sleep_sync_runtime.js");
+    var sync = require(__hooks + "/google_fit_sleep_runtime.js");
     return sync.settings(e);
 }, $apis.requireAuth("profiles"));
 
 routerAdd("POST", "/api/sleep-sync/run", function(e) {
-    var sync = require(__hooks + "/sleep_sync_runtime.js");
+    var sync = require(__hooks + "/google_fit_sleep_runtime.js");
     return sync.run(e);
 }, $apis.requireAuth("profiles"));
 
-cronAdd("lifeos_google_health_sleep_sync", "*/15 * * * *", function() {
-    var sync = require(__hooks + "/sleep_sync_runtime.js");
+cronAdd("lifeos_google_fit_sleep_sync", "*/15 * * * *", function() {
+    var sync = require(__hooks + "/google_fit_sleep_runtime.js");
     return sync.cron($app);
 });
