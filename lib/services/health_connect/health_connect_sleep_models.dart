02dfc141775d275f8262168c4318a8685577637a
nkuchenov-hash <nkuchenov@gmail.com>
2026-08-18 12:17:45 +0300

    Model detailed Health Connect sleep stages

--- lib/services/health_connect/health_connect_sleep_models.dart
+++ lib/services/health_connect/health_connect_sleep_models.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/foundation.dart';
 
 @immutable
+class HealthSleepStage {
+  const HealthSleepStage({
+    required this.startUtc,
+    required this.endUtc,
+    required this.stage,
+    required this.sourceId,
+    required this.sourceName,
+  });
+
+  final DateTime startUtc;
+  final DateTime endUtc;
+  final int stage;
+  final String sourceId;
+  final String sourceName;
+
+  Duration get duration => endUtc.difference(startUtc);
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'start': startUtc.toIso8601String(),
+        'end': endUtc.toIso8601String(),
+        'stage': stage,
+        'source': sourceId.isNotEmpty ? sourceId : sourceName,
+      };
+}
+
+@immutable
 class HealthSleepSession {
   const HealthSleepSession({
     required this.externalId,
@@ -8,6 +34,8 @@ class HealthSleepSession {
     required this.endUtc,
     required this.sourceId,
     required this.sourceName,
+    this.stages = const <HealthSleepStage>[],
+    this.recoveredFromStages = false,
   });
 
   final String externalId;
@@ -15,6 +43,8 @@ class HealthSleepSession {
   final DateTime endUtc;
   final String sourceId;
   final String sourceName;
+  final List<HealthSleepStage> stages;
+  final bool recoveredFromStages;
 
   Duration get duration => endUtc.difference(startUtc);
 }
