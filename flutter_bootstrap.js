3a69a20ebccec686237938cb0f7a439249129119
nkuchenov-hash <nkuchenov-hash@users.noreply.github.com>
2026-09-10 17:51:01 +0000

    Deploying to gh-pages from @ nkuchenov-hash/Counter@355153488c74cea25aa32558bcef09c2492ce378 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,6 +33,6 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=197bb528cbd9c22aab1e2dec2bb79d65efb03d82"}]};
+_flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=5c29f0d0c623d55b87e47732e65b02b0d9446767"}]};
 
 _flutter.loader.load();
